821b5eef84b2ee9ada49da80f95714994872c424
Foxboron <mcfoxax@gmail.com>
2012-12-03 10:29:56 +0100

    Fixes

--- app/views.py
+++ app/views.py
@@ -7,12 +7,14 @@ import json
 
 with open("config.conf", "r") as j:
     t = json.loads(j.read())
-
 s = GitHandler(t["Github"], t["Repo"], t["client_id"], t["client_secret"])
 s.fetch_posts()
+print "lol"
+
 
 @app.route('/')
 @app.route('/index')
+@app.route('/blog')
 def index():
     content = s.return_posts()
     return render_template("index.html", posts=content)
